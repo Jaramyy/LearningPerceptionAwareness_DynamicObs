e486680a654ff627ded755705572091cee2c3ce0
jaramyy <jamephet@gmail.com>
2025-05-10 23:34:25 +0700

    feat: add script for student inference

--- scripts/rl_games/play_imi_random.py
+++ scripts/rl_games/play_imi_random.py
@@ -86,115 +86,6 @@ import tempfile
 import h5py
 
 
-def train_student_policy(student_policy, dataloader, val_loader, optimizer, loss_fn, config, scheduler, checkpoint_dir='checkpoints'):
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    train_losses = []
-    learning_rates = []
-    best_val_loss = float('inf')
-    
-    for epoch in range(config['epochs']):
-        train_loss = []
-        current_lr = optimizer.param_groups[0]['lr']
-        learning_rates.append(current_lr)
-        
-        student_policy.train()
-
-        print(f"Training epoch {epoch+1}...")
-        print(f"Current LR: {current_lr}")
-
-        for obs_batch, action_batch in dataloader:
-            optimizer.zero_grad()
-            
-            # Replace NaNs in input data and ensure range consistency
-            # obs_batch = torch.nan_to_num(obs_batch, nan=0.0)
-            # action_batch = torch.nan_to_num(action_batch, nan=0.0)
-
-            # Check for NaNs or Infs and clamp values
-            if torch.isnan(obs_batch).any() or torch.isinf(obs_batch).any():
-                print("NaN or Inf detected in obs_batch. Skipping this batch.")
-                continue
-            if torch.isnan(action_batch).any() or torch.isinf(action_batch).any():
-                print("NaN or Inf detected in action_batch. Skipping this batch.")
-                continue
-
-            # Scale inputs and targets to manageable range
-            # obs_batch = torch.clamp(obs_batch, min=-10.0, max=10.0)  # need to normalize the data
-            # action_batch = torch.clamp(action_batch, min=-1.0, max=1.0)
-
-            # Forward pass
-            predictions = student_policy(obs_batch)
-            if torch.isnan(predictions).any() or torch.isinf(predictions).any():
-                print("NaN or Inf detected in predictions. Skipping this batch.")
-                continue
-
-            # Calculate loss
-            loss = loss_fn(predictions, action_batch)
-            if torch.isnan(loss):
-                print("NaN detected in loss. Skipping update.")
-                continue
-
-            # Backward pass
-            loss.backward()
-            # torch.nn.utils.clip_grad_norm_(student_policy.parameters(), max_norm=1.0)
-            optimizer.step()
-            train_loss.append(loss)
-        
-        avg_train_loss = torch.stack(train_loss).mean().item()
-        train_losses.append(avg_train_loss)
-
-        scheduler.step(avg_train_loss)
-
-        print(f"Epoch {epoch+1} train loss: {avg_train_loss:.4f}")
-
-
-        # Validation
-        student_policy.eval()
-        val_loss = []
-        with torch.no_grad():
-            for obs_batch, action_batch in val_loader:
-                # obs_batch = torch.clamp(obs_batch, min=-10.0, max=10.0)
-                # action_batch = torch.clamp(action_batch, min=-1.0, max=1.0)
-                predictions = student_policy(obs_batch)
-                loss = loss_fn(predictions, action_batch)
-                val_loss.append(loss)
-            
-            avg_val_loss = torch.stack(val_loss).mean().item()
-            val_loss.append(avg_val_loss)
-            print(f"Epoch {epoch+1} val loss: {avg_val_loss:.4f}")
-
-
-        # TODO: need to save model every 5 iterations
-        # Save current checkpoint
-        if (epoch + 1) % 5 == 0:
-            checkpoint_path = os.path.join(checkpoint_dir, f"checkpoint_epoch_{epoch+1}.pt")
-            torch.save({
-                'epoch': epoch + 1,
-                'model_state_dict': student_policy.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict(),
-                'train_loss': avg_train_loss,
-                'val_loss': avg_val_loss,
-            }, checkpoint_path)
-            print(f"Checkpoint saved at epoch {epoch+1} to {checkpoint_path}")
-
-        # Save best model
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
-            best_model_path = os.path.join(checkpoint_dir, "best_model.pt")
-            torch.save({
-                'epoch': epoch + 1,
-                'model_state_dict': student_policy.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict(),
-                'train_loss': avg_train_loss,
-                'val_loss': avg_val_loss,
-            }, best_model_path)
-            print(f"Saved new best model at epoch {epoch+1} with val loss {best_val_loss:.4f}")
-
-        wandb.log({'train_loss': avg_train_loss, 'val_loss': avg_val_loss, 'lr': current_lr})
-
-    return train_loss
-
 
 def play_student_model(student_policy, inputs):
     student_policy.eval()
@@ -350,7 +241,7 @@ def main():
     # dragger imitation
     # Load the checkpoint
     checkpoint_dir = os.path.join("checkpoints")
-    checkpoint_path = os.path.join(checkpoint_dir, log_dir, "best_model.pt")
+    checkpoint_path = os.path.join(checkpoint_dir, "best_model.pt")
     checkpoint = torch.load(checkpoint_path)
     # Load the model state
     # imitation_model = imitation.StudentPolicy()
@@ -364,144 +255,82 @@ def main():
     student_model.load_state_dict(checkpoint['model_state_dict'])
     student_model.eval()
 
-    optimizer = torch.optim.Adam(student_model.parameters(), lr=imitation.CONFIG_IMITATION['lr'])
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer,
-        'min',
-        factor=imitation.CONFIG_IMITATION['scheduler_factor'],
-        patience=imitation.CONFIG_IMITATION['scheduler_patience'],
-        min_lr=imitation.CONFIG_IMITATION['scheduler_min_lr']
-    )
-    loss_fn = imitation.StudentPolicy().loss()
+    # optimizer = torch.optim.Adam(student_model.parameters(), lr=imitation.CONFIG_IMITATION['lr'])
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    #     optimizer,
+    #     'min',
+    #     factor=imitation.CONFIG_IMITATION['scheduler_factor'],
+    #     patience=imitation.CONFIG_IMITATION['scheduler_patience'],
+    #     min_lr=imitation.CONFIG_IMITATION['scheduler_min_lr']
+    # )
+    # loss_fn = imitation.StudentPolicy().loss()
     
-    # Start wandb run
-    wandb.init(
-        project='training-imitation',
-        config=imitation.CONFIG_IMITATION,
-    )
-    # Log parameters and gradients
-    wandb.watch(student_model, log='all')
+    # # Start wandb run
+    # wandb.init(
+    #     project='training-imitation',
+    #     config=imitation.CONFIG_IMITATION,
+    # )
+    # # Log parameters and gradients
+    # wandb.watch(student_model, log='all')
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     file_name = f"dataset_{timestamp}.pt"
-    D = []
-    samples = {
-        "obs": [],
-        "action": [],
-        "reward": [],
-        "done": [],
-    }
     
     max_teacher_timesteps = 3000
     lidar_resolution = (60)
     lidar_range = 5.0
-    beta = 1.0
+    beta = 0.0  # Play only student model
     while simulation_app.is_running():
         start_time = time.time()
         
-        for iteration in tqdm(range(imitation.CONFIG_IMITATION['num_iterations']), desc="DAgger Iterations"):
+        with torch.inference_mode():
+            obs = agent.obs_to_torch(obs)
+            lin_vel = obs[:, :3]
+            ang_vel = obs[:, 3:6]
+            unit_desired_pos = obs[:, 6:9]
+            desired_dist_2d = obs[:, 9:11]
+            desired_dist_z = obs[:, 11:12]
 
-            with tqdm(total=imitation.CONFIG_IMITATION['num_steps'], desc="samples", unit="sample") as progress_bar:
-                new_data = []                
-                for step in range(0, imitation.CONFIG_IMITATION['num_steps']):
-                    
-                    with torch.inference_mode():
-                        obs = agent.obs_to_torch(obs)
-                        lin_vel = obs[:, :3]
-                        ang_vel = obs[:, 3:6]
-                        unit_desired_pos = obs[:, 6:9]
-                        desired_dist_2d = obs[:, 9:11]
-                        desired_dist_z = obs[:, 11:12]
+            lidar_scan = (
+                (
+                    env.env.scene["lidar_sensor"].data.ray_hits_w
+                    - env.env.scene["lidar_sensor"].data.pos_w.unsqueeze(1)
+                )
+                .norm(dim=-1)
+                .clamp_max(lidar_range)
+                .reshape(env.unwrapped.num_envs, 1, lidar_resolution)
+            )
+            drone_state = torch.cat((lin_vel, ang_vel, unit_desired_pos, desired_dist_2d, desired_dist_z), dim=1)
+            input_model = torch.cat((drone_state, lidar_scan.squeeze(1)), dim=1)  # shape (num_envs, 73)
+            # print("input model shape", input_model.shape)
+            student_input = input_model
+            teacher_actions = agent.get_action(obs, is_deterministic=agent.is_deterministic)
+            student_actions = play_student_model(student_model, student_input).squeeze(1)
 
-                        lidar_scan = (
-                            (
-                                env.env.scene["lidar_sensor"].data.ray_hits_w
-                                - env.env.scene["lidar_sensor"].data.pos_w.unsqueeze(1)
-                            )
-                            .norm(dim=-1)
-                            .clamp_max(lidar_range)
-                            .reshape(env.unwrapped.num_envs, 1, lidar_resolution)
-                        )
-                        drone_state = torch.cat((lin_vel, ang_vel, unit_desired_pos, desired_dist_2d, desired_dist_z), dim=1)
-                        input_model = torch.cat((drone_state, lidar_scan.squeeze(1)), dim=1)  # shape (num_envs, 73)
-                        # print("input model shape", input_model.shape)
-                        student_input = input_model
-                        teacher_actions = agent.get_action(obs, is_deterministic=agent.is_deterministic)
-                        student_actions = play_student_model(student_model, student_input).squeeze(1)
+            policy_actions = student_actions
+        
+            # new_data.extend(zip(student_input, teacher_actions))
+            # progress_bar.update(step - progress_bar.n)
 
-                        policy_actions = beta * teacher_actions + (1 - beta) * student_actions
-                    
-                        new_data.extend(zip(student_input, teacher_actions))
-                        progress_bar.update(step - progress_bar.n)
+            obs, reward , dones, _ = env.step(policy_actions)
 
-                        obs, reward , dones, _ = env.step(policy_actions)
-
-                        # perform operations for terminated episodes
-                        if len(dones) > 0:
-                            # reset rnn state for terminated episodes
-                            if agent.is_rnn and agent.states is not None:
-                                for s in agent.states:
-                                    s[:, dones, :] = 0.0
-                    if args_cli.video:
-                        timestep += 1
-                        # Exit the play loop after recording one video
-                        if timestep == args_cli.video_length:
-                            break
-                    # time delay for real-time evaluation
-                    sleep_time = dt - (time.time() - start_time)
-                    if args_cli.real_time and sleep_time > 0:
-                        print(f"Sleeping for {sleep_time:.2f} seconds to maintain real-time.")
-                        time.sleep(sleep_time)
+            # perform operations for terminated episodes
+            if len(dones) > 0:
+                # reset rnn state for terminated episodes
+                if agent.is_rnn and agent.states is not None:
+                    for s in agent.states:
+                        s[:, dones, :] = 0.0
+        if args_cli.video:
+            timestep += 1
+            # Exit the play loop after recording one video
+            if timestep == args_cli.video_length:
+                break
+        # time delay for real-time evaluation
+        sleep_time = dt - (time.time() - start_time)
+        if args_cli.real_time and sleep_time > 0:
+            print(f"Sleeping for {sleep_time:.2f} seconds to maintain real-time.")
+            time.sleep(sleep_time)
                 
-                D.extend(new_data)
-                print("size of dataset", len(D))
-                
-                # obs_np = np.stack([obs.cpu().numpy() for obs, act in D])
-                # act_np = np.stack([act.cpu().numpy() for obs, act in D])
-                # D = []
-                # new_data = []
-
-                # hdf5_path = './obs_buffer.h5'
-                # with h5py.File(hdf5_path, 'a') as f:
-                #     if 'observations' not in f:
-                #         f.create_dataset('observations', data=obs_np, maxshape=(None, obs_np.shape[1]), chunks=True)
-                #     else:
-                #         dset = f['observations']
-                #         old_size = dset.shape[0]
-                #         dset.resize((old_size + obs_np.shape[0], obs_np.shape[1]))
-                #         dset[old_size:] = obs_np
-
-                #     # Save or append actions
-                #     if 'actions' not in f:
-                #         f.create_dataset('actions', data=act_np, maxshape=(None, act_np.shape[1]), chunks=True)
-                #     else:
-                #         dset = f['actions']
-                #         old_size = dset.shape[0]
-                #         dset.resize((old_size + act_np.shape[0], act_np.shape[1]))
-                #         dset[old_size:] = act_np
-                # print("Data saved to HDF5 file.")
-
-                # imitation_dataset = imitation.HDF5DAggerDataset(hdf5_path)
-                # print("size of imitation dataset", len(imitation_dataset))
-                
-                dag_dataset = imitation.DAggerDataset(D)
-                train_size = int(0.8 * len(dag_dataset))
-                val_size = len(dag_dataset) - train_size
-                train_dataset, val_dataset = torch.utils.data.random_split(dag_dataset, [train_size, val_size])
-                
-                print("Dataloading datasets...")
-                train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=imitation.CONFIG_IMITATION['batch_size'], shuffle=True)
-                val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=imitation.CONFIG_IMITATION['batch_size'], shuffle=False)
-
-                # create_csv_from_dataset(train_loader, file_name)
-                train_student_policy(student_model, train_loader, val_loader, optimizer, loss_fn, imitation.CONFIG_IMITATION, scheduler)
-                # max_teacher_timesteps = max(0, round(max_teacher_timesteps - (iteration * 20)))   # adaptive teacher timesteps
-                beta = max(0.0, min(1.0, beta - (iteration * 0.001))) #0.01
-                # print(f"Beta: {beta:.5f}")
-                wandb.log({'beta': beta})
-               
-                
-
     # close the simulator
     env.close()
 
